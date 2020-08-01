285d140070fb3aa60a0e64d5d53f801f3797b323
oblivious-app <55861861+oblivious-app@users.noreply.github.com>
2020-07-31 22:05:05 -0700

    push the script

--- wither_skeleton_tower.py
+++ wither_skeleton_tower.py
@@ -5,7 +5,7 @@ gateway = JavaGateway(auto_convert=True)
 
 for i in range(3000):
     gateway.entry_point.put("nearby_mods", {"sid" : "1"})
-    sleep(2)
+    sleep(1)
     result = gateway.entry_point.get("1")
     if result and int(result["count"]) >= 1:
         gateway.entry_point.put("attack", {"x" : "4712", "y" : "40", "z" : "-3372"})
