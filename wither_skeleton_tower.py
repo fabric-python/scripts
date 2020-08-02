cb1eec9c491e2715385d50dfc8ffe94a852002ba
oblivious-app <55861861+oblivious-app@users.noreply.github.com>
2020-08-01 19:19:32 -0700

    push

--- wither_skeleton_tower.py
+++ wither_skeleton_tower.py
@@ -2,13 +2,33 @@ from time import sleep
 from py4j.java_gateway import JavaGateway
 
 gateway = JavaGateway(auto_convert=True)
+mc = gateway.entry_point
+
+res = mc.put_and_get("switch_items", {"name" : "minecraft:netherite_sword"})
+if res and res["res"] == "failure":
+    mc.put_and_get("switch_items", {"name": "minecraft:diamond_sword"})
+
+default = {"default" : ""}
 
 for i in range(3000):
-    gateway.entry_point.put("nearby_mods", {"sid" : "1"})
-    sleep(1)
-    result = gateway.entry_point.get("1")
-    if result and int(result["count"]) >= 1:
-        gateway.entry_point.put("attack", {"x" : "4712", "y" : "40", "z" : "-3372"})
+    nearby_wither_skeletons = mc.put_and_get("nearby_mods", {"type" : "wither_skeleton"})
+
+    if nearby_wither_skeletons and int(nearby_wither_skeletons["count"]) >= 1:
+        mc.put_and_get("attack", {"type" : "wither_skeleton", "x": "4712", "y": "40", "z": "-3372"})
         sleep(1)
-        gateway.entry_point.put("attack", {"x" : "4712", "y" : "40", "z" : "-3372"})
-    sleep(1)
+
+    nearby_magma_cubes = mc.put_and_get("nearby_mods", {"type": "magma_cube"})
+
+    if nearby_magma_cubes and int(nearby_magma_cubes["count"] >= 1):
+        mc.put_and_get("attack", {"type": "nearby_magma_cubes", "x": "4712", "y": "40", "z": "-3372"})
+        sleep(1)
+
+    hungry = mc.put_and_get("hungry", default)
+    if hungry and int(hungry["res"]) <= 12:
+        mc.put_and_get("switch_items", {"name": "minecraft:cooked_porkchop"})
+        mc.put_and_get("use", default)
+        sleep(2)
+        res = mc.put_and_get("switch_items", {"name": "minecraft:netherite_sword"})
+        if res and res["res"] == "failure":
+            mc.put_and_get("switch_items", {"name": "minecraft:diamond_sword"})
+        sleep(1)
